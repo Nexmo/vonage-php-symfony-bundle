38e1a5bc3c0903bd46164906860942ed1f4b97bc
Chris Tankersley <chris@ctankersley.com>
2020-11-19 17:19:19 -0500

    Added additional auth methods

--- VonageClientFactory.php
+++ VonageClientFactory.php
@@ -6,6 +6,8 @@ use Vonage\Client;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Vonage\Client\Credentials\Container;
 use Vonage\Client\Credentials\Basic;
+use Vonage\Client\Credentials\Keypair;
+use Vonage\Client\Credentials\SignatureSecret;
 
 class VonageClientFactory
 {
@@ -13,12 +15,26 @@ class VonageClientFactory
     {
         $config = $container->getParameter('vonage_client');
         $credentials = [];
-        if (!empty($config['api_key']) && !empty($config['api_secret'])) {
-            $credentials[] = new Basic($config['api_key'], $config['api_secret']);
+        if (!empty($config['api_key'])) {
+            if (!empty($config['api_secret'])) {
+                $credentials[] = new Basic($config['api_key'], $config['api_secret']);
+            }
+
+            if (!empty($config['signature_secret']) && !empty($config['signature_method'])) {
+                $credentials[] = new SignatureSecret($config['api_key'], $config['signature_secret'], $config['signature_method']);
+            }
+        }
+
+        if (!empty($config['application_id'])) {
+            if (!empty($config['private_key_path'])) {
+                $credentials[] = new Keypair(file_get_contents($config['private_key_path']), $config['application_id']);
+            } elseif (!empty($config['private_key'])) {
+                $credentials[] = new Keypair($config['private_key'], $config['application_id']);
+            }
         }
 
         if (empty($credentials)) {
-            throw new \RuntimeException('Must set vonage credentials to use Vonage Client');
+            throw new \RuntimeException('Must set Vonage credentials to use Vonage Client');
         }
 
         $credsContainer = new Container($credentials);
